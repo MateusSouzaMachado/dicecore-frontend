ee7f409ab2171b781b824f4a0128810067b665d3
Mateus Souza Machado <mateus2005machado@gmail.com>
2025-05-27 21:47:38 -0300

    123

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,16 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-container">
+    <div className="hero-section">
       <h1>Bem-vindo ao DiceCore</h1>
-      <p>Um sistema de RPG online para mestres e jogadores se conectarem, criarem personagens e registrarem aventuras!</p>
-      <img src="/rpg-banner.jpg" alt="Banner RPG" className="home-banner" />
+      <p>Um sistema de RPG online para mestres e jogadores se conectarem...</p>
+      <img src="/dadoEstilhacado.jpg-removebg-preview.png" alt="Logo Estilhaços" className="logo-img" />
     </div>
+  
+    <div className="content">
+      {/* Aqui começa o restante da página */}
+    </div>
+  </div>
   );
 }
 
